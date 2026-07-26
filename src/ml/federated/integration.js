fce51dd6657d492b1e37efb111de2b29424f8cca
Theraxia <osawumeseeg@gmail.com>
2026-07-26 06:16:42 +0000

    Resolve merge conflicts from stash

--- src/ml/federated/integration.js
+++
@@ -1,4 +0,0 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const { FederatedLearningIntegration } = require('./integration.cjs')
-export { FederatedLearningIntegration }
--- src/ml/federated/server.js
+++
@@ -1,4 +0,0 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const { FederatedServer } = require('./server.cjs')
-export { FederatedServer }
